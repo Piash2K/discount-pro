54c3c91de7b4e024d6aab9b947b9f71494956f32
PIASH <zmnobab1999@gmail.com>
2024-11-21 11:16:53 +0600

    React stars added to brands route and some others changes done

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import { Link, useLoaderData, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../provider/AuthProvider';
 import 'animate.css';
+import ReactStars from 'react-stars';
 
 const Brands = () => {
     const { user } = useContext(AuthContext);
@@ -24,7 +25,7 @@ const Brands = () => {
     return (
         <div className="min-h-screen bg-gray-100 p-6">
             {/* Page Title */}
-            <h1 className="text-4xl font-bold text-center mb-6 animate-bounce">Our Partner Brands</h1>
+            <h1 className="text-4xl font-bold text-center mb- ">Our Partner Brands</h1>
 
             {/* Search Bar */}
             <div className="flex justify-center mb-6">
@@ -42,40 +43,60 @@ const Brands = () => {
                 {filteredBrands.map((brand) => (
                     <div
                         key={brand._id}
-                        className="bg-white shadow-md rounded-lg p-4 border flex items-center space-x-4"
+                        className="bg-white shadow-md rounded-lg p-4 border grid grid-cols-3 items-center space-x-4"
                     >
                         {/* Brand Logo */}
-                        <img
-                            src={brand.brand_logo}
-                            alt={`${brand.brand_name} Logo`}
-                            className="w-24 h-24  rounded-lg"
-                        />
+                        <div className='flex flex-col'>
+                            <div>
+                                <img
+                                    src={brand.brand_logo}
+                                    alt={`${brand.brand_name} Logo`}
+                                    className="w-32 h-24  rounded-lg"
+                                />
+                            </div>
+                            <div>
+                                <h2 className="text-xl font-semibold">{brand.brand_name}</h2>
+                                <div className='my-2'>
+                            <h4 className='font-bold'>Rating:</h4>
+                            <div className='flex gap-4 items-center'>
+                                <ReactStars
+                                    count={5}
+                                    value={brand.rating}
+                                    size={24}
+                                    activeColor="#ffd700"
+                                    edit={false}
+                                />
+                                <p>{brand.rating}</p>
+                            </div>
+                        </div>
+                            </div>
+                        </div>
 
                         {/* Brand Info */}
-                        <div className="flex-grow">
-                            <div className="flex items-center space-x-2 mb-2">
+                        <div className="flex flex-col justify-start">
+                            <div>
                                 <span className="text-xl font-semibold">{brand.brand_name}</span>
-                                <span className="flex items-center text-yellow-500">
-                                    <i className="fas fa-star"></i>
-                                    <span className="ml-1">{brand.rating}</span>
-                                </span>
                             </div>
                             <p className="text-gray-600">{brand.description}</p>
                         </div>
 
                         {/* Buttons */}
-                        <div className="text-center">
-                            {brand.saleIsOn && (
-                                <p className="text-red-500 font-bold animate-bounce mb-2">
-                                    Sale is on!
-                                </p>
-                            )}
-                            <button
-                                onClick={() => handleViewCoupons(brand._id)}
-                                className="btn btn-primary"
-                            >
-                                View Coupons
-                            </button>
+                        <div className="flex justify-end text-center">
+
+                            <div className='flex flex-col'>
+                                <div
+                                    className={`${brand.isSaleOn ? "text-purple-500 animate-bounce" : "text-gray-400"
+                                        } text-sm font-semibold`}
+                                >
+                                    {brand.isSaleOn ? "Sale On" : ""}
+                                </div>
+                                <button
+                                    onClick={() => handleViewCoupons(brand._id)}
+                                    className="btn btn-primary  ">
+                                    View Coupons
+                                </button>
+
+                            </div>
                         </div>
                     </div>
                 ))}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import Marquee from "react-fast-marquee";
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useNavigate } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import darazbanner from '../assets/daraz-banner.jpg'
 import pickaboobanner from '../assets/pickaboo-banner.jpg'
 import ajkerdeal from '../assets/Ajker-deal.jpg'
 import Foodpanda from '../assets/Foodpanda.png'
+import { AuthContext } from "../provider/AuthProvider";
+import 'animate.css';
 
 const Home = () => {
+    const { user } = useContext(AuthContext)
     const brandsData = useLoaderData();
     const brandsForSell = brandsData.filter(brands => brands.isSaleOn === true)
+    const navigate = useNavigate()
     // console.log(brandsForSell)
+    const handleViewBrand = (_id) => {
+        if (user) {
+            navigate(`brands/brand/${_id}`);
+        } else {
+            navigate('/login');
+        }
+    };
+
     return (
         <div>
             <div className="w-8/ mx-auto my-10">
@@ -82,50 +94,23 @@ const Home = () => {
                 <h2 className="text-center text-4xl font-bold">Top Brands</h2>
                 <div className="my-10">
                     <Marquee pauseOnHover>
-                        <div className='flex gap-12'>
-                            <Link to='/brands/brand/c1d2e3f4g5h6'>
-                                <img className="w-[150px] h-[100px] ml-12" src="https://i.ibb.co.com/sjV8wCm/Daraz-Logo.png" alt="" />
-                            </Link>
-                            <Link to='/brands/brand/i7j8k9l0m1n2'>
-                                <img className="w-[150px] h-[100px]" src="https://i.ibb.co.com/Bj6yS62/pickabologo.png" alt="" />
-                            </Link>
-                            <Link to='/brands/brand/o3p4q5r6s7t8'>
-                                <img className="w-[150px] h-[100px]" src="https://i.ibb.co.com/ZgcHRds/Chaldal.png" alt="" />
-                            </Link>
-                            <Link to='/brands/brand/u9v0w1x2y3z4'>
-                                <img className="w-[150px] h-[100px]" src="https://i.ibb.co.com/PtnygJD/ajker-deal.png" alt="" />
-                            </Link>
-                            <Link to='/brands/brand/a5b6c7d8e9f0'>
-                                <img className="w-[150px] h-[100px]" src="https://i.ibb.co.com/mB596cr/Foodpandalogo.png" alt="" />
-                            </Link>
-                            <Link to='/brands/brand/g1h2i3j4k5l6'>
-                                <img className="w-[150px] h-[100px]" src="https://i.ibb.co.com/wY8C2nJ/rokomari.png" alt="" />
-                            </Link>
-                            <Link to='/brands/brand/m7n8o9p0q1r2'>
-                                <img className="w-[150px] h-[100px]" src="https://i.ibb.co.com/C5t5p71/bagdoom.jpg" alt="" />
-                            </Link>
-                            <Link to='/brands/brand/s3t4u5v6w7x8'>
-                                <img className="w-[150px] h-[100px]" src="https://i.ibb.co.com/cJXDzrs/shajgojlogo.png" alt="" />
-                            </Link>
-                            <Link to='/brands/brand/t8u9v0w1x2y3'>
-                                <img className="w-[150px] h-[100px]" src="https://i.ibb.co.com/Ws4Lf6z/png-clipart-bangladesh-industrial-and-technical-assistance-center-pathao-business-service-business-t.png" alt="" />
-                            </Link>
-                            <Link to='/brands/brand/x3y4z5a6b7c8'>
-                                <img className="w-[150px] h-[100px]" src="https://i.ibb.co.com/Kw0WNgj/bikroy.png" alt="" />
-                            </Link>
-                            <Link to='/brands/brand/y9z0a1b2c3d4'>
-                                <img className="w-[150px] h-[100px]" src="https://i.ibb.co.com/dGhP0CW/priyoshop-logopng.png" alt="" />
-                            </Link>
-                            <Link to='/brands/brand/e5f6g7h8i9j0'>
-                                <img className="w-[150px] h-[100px]" src="https://i.ibb.co.com/r2pZ8WJ/techland.webp" alt="" />
-                            </Link>
+                        <div className="bg-white shadow-md rounded-lg p-4 border flex items-center space-x-4">
+                        {brandsData.map((brand) => (
+                            <div
+                                key={brand._id}>
+                                <img onClick={() => handleViewBrand(brand._id)}
+                                    src={brand.brand_logo}
+                                    alt={`${brand.brand_name} Logo`}
+                                    className="w-[100px] h-[60px] ml-12 rounded-lg"></img>
+                            </div>
+                        ))}
 
                         </div>
                     </Marquee>
                 </div>
             </div>
             <div>
-                <h2 className='text-center text-4xl font-bold'>Brands on sell</h2>
+                <h2 className='text-center text-4xl font-bold animate__fadeInRightBig'>Brands on sell</h2>
                 <div className="grid grid-cols-3 gap-6">
                     {brandsForSell.map((brand) => (
                         <div
